9ae470e043fb262b7e54a6fae59f45c4b9279ebf
Nevylish <36775882+Nevylish@users.noreply.github.com>
2021-12-18 16:13:34 +0000

    semicolon missing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express'),
-  app = express()
+  app = express(),
   lyricsFinder = require('lyrics-finder');
 
 const isAuthorized = (req,res, next) => {
@@ -9,15 +9,15 @@ const isAuthorized = (req,res, next) => {
     } else {
       res.status(401);
       res.send("You do not have permission to access this page.");
-    }
-}
+    };
+};
 
 app.use(express.json());
 
 app.get('/lyricsfinder/:request', isAuthorized, async (req, res) => {
   const lyricsToFind = req.params.request;
-  let lyrics = await lyricsFinder(lyricsToFind)
-  res.status(200).json({lyrics: lyrics})
+  let lyrics = await lyricsFinder(lyricsToFind);
+  res.status(200).json({lyrics: lyrics});
 });
 
 app.listen(3000);
