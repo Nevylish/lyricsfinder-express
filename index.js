bb2c4480a1f1dbf6d27cb167316968e37f76abdc
Nevylish <nevylish@pm.me>
2022-02-16 10:43:52 +0100

    format

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-const express = require('express'),
+const express = require("express"),
   app = express(),
-  lyricsFinder = require('lyrics-finder');
+  lyricsFinder = require("lyrics-finder");
 
 const isAuthorized = (req, res, next) => {
   const auth = req.headers.authorization;
-    if (auth === process.env.AUTHPASS) {
-      next();
-    } else {
-      res.status(401);
-      res.send("You do not have permission to access this page.");
-    }
-}
+  if (auth === process.env.AUTHPASS) {
+    next();
+  } else {
+    res.status(401);
+    res.send("You do not have permission to access this page.");
+  }
+};
 
-app.get('/', isAuthorized, async (req, res) => {
-  res.status(404).json({error: "You must put a music like this: https://lyricsfinder.api.nevylish.fr/All I Want For Christmas Is You - Mariah Carey"});
+app.get("/", isAuthorized, async (req, res) => {
+  res
+    .status(404)
+    .json({
+      error:
+        "You must put a music like this: https://lyricsfinder.api.nevylish.fr/All I Want For Christmas Is You - Mariah Carey",
+    });
 });
 
-app.get('/:request', isAuthorized, async (req, res) => {
+app.get("/:request", isAuthorized, async (req, res) => {
   const lyricsToFind = req.params.request;
   let lyrics = await lyricsFinder(lyricsToFind);
 
-  if (lyrics) 
-    res.status(200).json({lyrics: lyrics});
-  else
-    res.status(404).json({error: "Lyrics not found."})
+  if (lyrics) res.status(200).json({ lyrics: lyrics });
+  else res.status(404).json({ error: "Lyrics not found." });
 });
 
 app.listen(3000);
